dcede2b98aae06a5a73f4bac02d7849accc2b476
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-01-30 00:06:42 -0300

    Primer servidor con users

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Crear servidor web 
 
 const express = require('express');
-const app = express() //Creamos una instancia de express  
+const app = express(); //Creamos una instancia de express  
 
 
 // app.get(); //READ
@@ -9,7 +9,24 @@ const app = express() //Creamos una instancia de express
 // app.put(); //UPDATE
 // app.delete(); //DELETE 
 
+// Función callback 
+
+app.get('/', (req, res) => {
+
+res.send('Bienvenides chiques como están todo bien')
+
+}) 
+
+app.get('/api/users', (req, res) => {
 
 
- 
+res.send(['Axel', 'Agus', 'Pablo'])
 
+
+});
+
+app.listen(3000, () => {
+
+console.log("Escuchando en el puerto 3000...")
+
+})
