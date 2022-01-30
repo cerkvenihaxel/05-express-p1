e42ebdaae58b17c03e51376f7b19ae2333adb105
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-01-30 02:45:51 -0300

    Modificación con PUT

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //Crear servidor web 
 
 const express = require('express');
+const {
+    use
+} = require('express/lib/application');
+const req = require('express/lib/request');
 const app = express(); //Creamos una instancia de express  
 const users = [
 
@@ -18,6 +22,19 @@ const users = [
     }
 
 ] // Arreglo de usuarios 
+
+//Validacion de datos 
+
+const Joi = require('joi');
+
+const schema = Joi.object({
+    nombre: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+
+});
+
 
 app.use(express.json());
 
@@ -54,19 +71,60 @@ app.get('/api/users/:id', (req, res) => {
 
 app.post('/api/users', (req, res) => {
 
-    if(!req.body.nombre || req.body.nombre.length <= 2) {
-        res.status(400).send('Debe ingresar un nombre con mas de 3 letras'); //400 Bad Request - respuesta al cliente
-        return;
-    }
-    const usuario = {
 
-        id: users.length + 1,
+    const {
+        error,
+        value
+    } = schema.validate({
         nombre: req.body.nombre
+    });
+    if (!error) {
+        const usuario = {
 
-    };
+            id: users.length + 1,
+            nombre: value.nombre
+        }
 
-    users.push(usuario);
-    res.send(usuario);
+        users.push(usuario);
+        res.send(usuario);
+
+    } else {
+        const msj = error.details[0].message;
+        res.status(400).send(msj)
+    }
+
+
+});
+
+app.put('/api/users/:id', (req, res) => {
+    //Validación para encontrar si es que existe el usuario a modificar  
+
+    let usuario = users.find(u => u.id === parseInt(req.params.id))
+    if (!usuario) res.status(404).send('Disculpa capo el usuario no se encontró')
+    res.send(`El usuario ${usuario.id} es ${usuario.nombre}`)
+
+
+    const {
+        error,
+        value
+    } = schema.validate({
+        nombre: req.body.nombre
+    });
+    if (!error) {
+        const usuario = {
+
+            id: users.length + 1,
+            nombre: value.nombre
+        }
+
+        users.push(usuario);
+        res.send(usuario);
+
+    } else {
+        const msj = error.details[0].message;
+        res.status(400).send(msj)
+    }
+
 });
 
 
