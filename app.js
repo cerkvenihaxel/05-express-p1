6d45a2f48d8109072d4c62dfcf6112800c7f26c2
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-01-29 23:57:19 -0300

    Metodos que podemos utilizar

---
+++ app.js
@@ -0,0 +1,15 @@
+//Crear servidor web 
+
+const express = require('express');
+const app = express() //Creamos una instancia de express  
+
+
+// app.get(); //READ
+// app.post(); //CREATE
+// app.put(); //UPDATE
+// app.delete(); //DELETE 
+
+
+
+ 
+
