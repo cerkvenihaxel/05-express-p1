29c89a7f0fc0eadf62fa222432e517d13b289eab
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-01-30 02:22:31 -0300

    Validacion de datos

--- app.js
+++ app.js
@@ -2,7 +2,24 @@
 
 const express = require('express');
 const app = express(); //Creamos una instancia de express  
+const users = [
 
+    {
+        id: 1,
+        nombre: 'Axel'
+    },
+    {
+        id: 2,
+        nombre: 'Agus'
+    },
+    {
+        id: 3,
+        nombre: 'Anita'
+    }
+
+] // Arreglo de usuarios 
+
+app.use(express.json());
 
 // app.get(); //READ
 // app.post(); //CREATE
@@ -25,11 +42,32 @@ app.get('/api/users', (req, res) => {
 
 });
 
-app.get('/api/users/:year/:month', (req, res) => {
+app.get('/api/users/:id', (req, res) => {
 
-    res.send(req.params);
+    let usuario = users.find(u => u.id === parseInt(req.params.id))
 
-}) 
+    if (!usuario) res.status(404).send('Disculpa capo el usuario no se encontró')
+    res.send(`El usuario ${usuario.id} es ${usuario.nombre}`)
+
+})
+
+
+app.post('/api/users', (req, res) => {
+
+    if(!req.body.nombre || req.body.nombre.length <= 2) {
+        res.status(400).send('Debe ingresar un nombre con mas de 3 letras'); //400 Bad Request - respuesta al cliente
+        return;
+    }
+    const usuario = {
+
+        id: users.length + 1,
+        nombre: req.body.nombre
+
+    };
+
+    users.push(usuario);
+    res.send(usuario);
+});
 
 
 const port = process.env.PORT || 3000; //Creamos un a variable de entorno si existe el puerto, si no, en el puerto 3000
